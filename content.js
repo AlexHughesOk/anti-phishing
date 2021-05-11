c8731071742d66af9600ac9d95e2f3c8a71b6790
Alex <57485304+AlexHughesOk@users.noreply.github.com>
2021-05-11 21:15:46 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,39 +1,62 @@
-var z = 0;
+// Comment code out CTRL+K+C, uncomment CTRL+K+U
 
-var hosts = [];
+var intervalId = -1;
 
-for (i = 0; i < top.document.links.length; i++) {
-    var host = top.document.links[i].hostname;
-      host = host.toLowerCase();
+//Removes Alert if clicked
+document.addEventListener('click', WindowClick);
+function WindowClick() {
 
-      // For Outlook Links
-      if (host.search(/safelinks\.protection\.outlook\.com/i) >= 0) {
+  getAllLink()
+}
 
-          host = top.document.links[i].href.replace(/https:\/\/\w*\.?safelinks\.protection\.outlook\.com\/\?url=/gmi, "");
-          host = decodeURIComponent(host);
-          host = host.replace(/^https?:\/\//gmi, "");
-          host = host.replace(/^www\./gmi, "");
-          host = host.replace(/[\?\&\/].*/gmi, "");
-      }
 
-    host = host.replace(/^www\./gmi, "");
 
-    if (hosts.indexOf(host) == -1 && host.match(/\.[a-z]*$/)) {
-      hosts.push(host);
-    }
+
+// https://stackoverflow.com/questions/27590366/how-to-get-all-links-in-a-page-by-classname-and-store 
+
+function getAllLink(){
+  var class1 = document.getElementsByClassName('AO');
+  var array1 = [];
+  for(i=0;i<class1.length;i++){
+      var str = class1[i].getElementsByTagName('a')[0].href;
+      var res = str.split("?"); 
+      array1.push(res[0]);
+  }
+  //console.log(array1);
+  window.alert(array1)
+}
+
+//https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
+
+function extractHostname(url) {
+  var hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+
+  if (url.indexOf("//") > -1) {
+      hostname = url.split('/')[2];
+  }
+  else {
+      hostname = url.split('/')[0];
   }
 
-  console.log(hosts)
-  console.log(host.search)
+  //find & remove port number
+  hostname = hostname.split(':')[0];
+  //find & remove "?"
+  hostname = hostname.split('?')[0];
+
+  return hostname;
+}
+
+
+
+
 
 // Shows the Alert
 function showAlert() {
 
     window.alert("Working");
-
   }
 
 // Removes the Alert
   function RemoveAlert() {
-    
   }
