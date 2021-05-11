50ed5320c6d317b96ef4fdbaac636609e17a1a45
Alex <57485304+AlexHughesOk@users.noreply.github.com>
2021-05-11 21:13:21 +0100

    Update content.js

--- content.js
+++ content.js
@@ -22,11 +22,30 @@ function getAllLink(){
       var res = str.split("?"); 
       array1.push(res[0]);
   }
-  console.log(array1);
+  //console.log(array1);
   window.alert(array1)
 }
 
-window.alert(array1)
+//https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
+
+function extractHostname(url) {
+  var hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+
+  if (url.indexOf("//") > -1) {
+      hostname = url.split('/')[2];
+  }
+  else {
+      hostname = url.split('/')[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(':')[0];
+  //find & remove "?"
+  hostname = hostname.split('?')[0];
+
+  return hostname;
+}
 
 
 
