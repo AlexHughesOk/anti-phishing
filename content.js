7acbcc8dd3fdf146da8d03bb6462326607f06179
Alexander Hughes <a.hughes5878@student.leedsbeckett.ac.uk>
2021-05-22 16:06:44 +0100

    Warning popup created, with close function

--- content.js
+++ content.js
@@ -1,60 +1,75 @@
 // Comment code out CTRL+K+C, uncomment CTRL+K+U
 
-var intervalId = -1;
+// var intervalId = -1;
 
 //Removes Alert if clicked
-document.addEventListener('click', WindowClick);
-function WindowClick() {
+// document.addEventListener('click', WindowClick);
+// function WindowClick() {
 
-  getAllLink()
-}
+//   getAllLink()
+// }
 
 if(document.URL.indexOf("https://mail.google.com/") >= 0){ 
   //Run functions on Gmail
-  getAllLink()
+  gmailGetAllLinks()
+
   }
 else if(document.URL.indexOf("https://outlook.live.com/") >= 0) {
   //Run functions on Outlook
+  
 }
 
 
 
 // https://stackoverflow.com/questions/27590366/how-to-get-all-links-in-a-page-by-classname-and-store 
-function getAllLink(){
-  var class1 = document.getElementsByClassName('AO');
+function gmailGetAllLinks(){
+  var classname = document.getElementsByClassName('AO');
   var array1 = [];
-  for(i=0;i<class1.length;i++){
-      var str = class1[i].getElementsByTagName('a')[0].href;
+  for(i=0;i<classname.length;i++){
+      var str = classname[i].getElementsByTagName('a')[0].href;
       var res = str.split("?"); 
       array1.push(res[0]);
+
   }
   //console.log(array1);
-  window.alert(array1)
-}
-
-//https://stackoverflow.com/questions/8498592/extract-hostname-name-from-string
-
-function extractHostname(url) {
-  var hostname;
-  //find & remove protocol (http, ftp, etc.) and get hostname
-
-  if (url.indexOf("//") > -1) {
-      hostname = url.split('/')[2];
-  }
-  else {
-      hostname = url.split('/')[0];
-  }
-
-  //find & remove port number
-  hostname = hostname.split(':')[0];
-  //find & remove "?"
-  hostname = hostname.split('?')[0];
-
-  return hostname;
+  //window.alert(array1)
+  warningPopup()
 }
 
 
+function warningPopup () {
+  var defaultCss = document.createElement('style');
+  defaultCss.setAttribute('type', 'text/css');
+  var css = '';
 
+  defaultCss.innerHTML = css;
+  document.head.appendChild(defaultCss);
+  var divAlert = document.createElement('div');
+  divAlert.innerHTML = "Warning this email could be dangerous!";
+
+  // CSS for the shape of the box
+  document.body.appendChild(divAlert);
+  divAlert.id = 'warningAlert';
+  divAlert.style.position = 'absolute';
+  divAlert.style.top = '25px';
+  divAlert.style.right = '25px';
+  divAlert.style.background = 'rgba(255, 0, 0)';
+  divAlert.style.borderRadius = '5px';
+  divAlert.style.zIndex = '101';
+  divAlert.style.padding = '15px';
+  divAlert.style.fontFamily = 'Product Sans';
+  divAlert.style.fontSize = '12px';
+
+  var phishingWebsites = document.createElement('div');
+ // phishingWebsites.innerHTML = PHISHING VAR GOES HERE;
+  divAlert.appendChild(phishingWebsites);
+
+  //Cancel Button
+  var cancel = document.createElement('div');
+  cancel.innerHTML = 'close';
+  cancel.onclick = function (e) { divAlert.parentNode.removeChild(divAlert) };                                  
+  divAlert.appendChild(cancel);
+}
 
 
 // Shows the Alert
