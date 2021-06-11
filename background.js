b4bb5ea17c9393ace32fccc86e1963c98b6606d7
Alexander Hughes <a.hughes5878@student.leedsbeckett.ac.uk>
2021-06-11 06:16:25 +0100

    ID changes per machine

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.local.get('deviceID', function(result) {
-      if (!result.deviceID) {
-          chrome.tabs.create({
-              url: "chrome-extension://blimlhmjgcnlihkfblolambkgjkchlnd/html/infoPage.html"
-          });
-      };
-  });
-});
+// This code only works on MY machine as the 'blimlhmjgcnlihkfblolambkgjkchlnd' changes per computer. 
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.local.get('deviceID', function(result) {
+//       if (!result.deviceID) {
+//           chrome.tabs.create({
+//               url: "chrome-extension://blimlhmjgcnlihkfblolambkgjkchlnd/html/infoPage.html"
+//           });
+//       };
+//   });
+// });
