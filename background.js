1fecc23a80edb5a90dda2bc3b24447a4e65d0e5c
Alexander Hughes <a.hughes5878@student.leedsbeckett.ac.uk>
2021-06-11 06:21:19 +0100

    FIXES

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
-// This code only works on MY machine as the 'blimlhmjgcnlihkfblolambkgjkchlnd' changes per computer. 
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.storage.local.get('deviceID', function(result) {
-//       if (!result.deviceID) {
-//           chrome.tabs.create({
-//               url: "chrome-extension://blimlhmjgcnlihkfblolambkgjkchlnd/html/infoPage.html"
-//           });
-//       };
-//   });
-// });
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.local.get('deviceID', function(result) {
+      if (!result.deviceID) {
+          chrome.tabs.create({
+              url: "chrome-extension://blimlhmjgcnlihkfblolambkgjkchlnd/html/infoPage.html"
+          });
+      };
+  });
+});
